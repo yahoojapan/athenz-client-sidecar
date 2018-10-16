1b53c93b47a6ead6cf756a7ed9be17791c2533d4
Wanpan Diu <wdiu@yahoo-corp.jp>
2018-10-16 10:55:19 +0900

    Add model.go comments (#7)

--- model/model.go
+++ model/model.go
@@ -2,23 +2,47 @@ package model
 
 import "time"
 
+// HCRequest represent the Host Certificate request information.
 type HCRequest struct {
+	// AppID represent Host Certificate application ID.
 	AppID string `json:"app_id"`
 }
 
+// UDBRequest represent the User Database request information.
 type UDBRequest struct {
-	AppID   string   `json:"app_id"`
-	NCookie string   `json:"n_cookie"`
-	TCookie string   `json:"t_cookie"`
-	KeyID   string   `json:"key_id"`
-	KeyData string   `json:"key_data"`
-	Keys    []string `json:"keys"`
+	// AppId represent User Database application ID.
+	AppID string `json:"app_id"`
+
+	// NCookie represent the N-Cookie string to connect to User Database.
+	NCookie string `json:"n_cookie"`
+
+	// TCookie represent the T-Cookie string to connect to User Database.
+	TCookie string `json:"t_cookie"`
+
+	// KeyID represent the Key ID to connect to User Database.
+	KeyID string `json:"key_id"`
+
+	// KeyData represent the Key Data to connect to User Database.
+	KeyData string `json:"key_data"`
+
+	// Keys represent the elements get from User Database.
+	Keys []string `json:"keys"`
 }
 
+// RoleRequest represent the request information to get the role token.
 type RoleRequest struct {
-	Domain            string        `json:"domain"`
-	Role              string        `json:"role"`
-	ProxyForPrincipal string        `json:"proxy_for_principal"`
-	MinExpiry         time.Duration `json:"min_expiry"`
-	MaxExpiry         time.Duration `json:"max_expiry"`
+	// Domain represent the domain field of the request.
+	Domain string `json:"domain"`
+
+	// Role represent the role field of the request.
+	Role string `json:"role"`
+
+	// ProxyForPrincipal represent the ProxyForPrincipal field of the request.
+	ProxyForPrincipal string `json:"proxy_for_principal"`
+
+	// MinExpiry represent the MinExpiry field of the request.
+	MinExpiry time.Duration `json:"min_expiry"`
+
+	// MaxExpiry represent the MaxExpiry field of the request.
+	MaxExpiry time.Duration `json:"max_expiry"`
 }

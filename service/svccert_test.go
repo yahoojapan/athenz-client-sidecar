4ec2a12bf9e4533dc58acfb448c9cce2526ab821
Wataru Nagai <wnagai@yahoo-corp.jp>
2019-09-04 16:31:58 +0900

    fix not to load svccert when go routine starts / fix tests

--- service/svccert_test.go
+++ service/svccert_test.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"net/http"
 	"testing"
 	"time"
 
@@ -23,6 +22,7 @@ func TestNewSvcCertService(t *testing.T) {
 		name      string
 		args      args
 		want      SvcCertService
+		wantErr   error
 		checkfunc func(*svcCertService, *svcCertService) bool
 	}
 
@@ -30,13 +30,14 @@ func TestNewSvcCertService(t *testing.T) {
 		func() test {
 			dur, _ := time.ParseDuration("30m")
 			token := func() (string, error) { return "", nil }
-			tokenCfg := config.Token{}
 
 			return test{
 				name: "Success to initialize SvcCertService",
 				args: args{
 					cfg: config.Config{
-						Token: tokenCfg,
+						Token: config.Token{
+							PrivateKeyPath: "./assets/dummyServer.key",
+						},
 						ServiceCert: config.ServiceCert{
 							AthenzRootCA:    "./assets/dummyCa.pem",
 							RefreshDuration: "30m",
@@ -49,24 +50,25 @@ func TestNewSvcCertService(t *testing.T) {
 						AthenzRootCA:    "./assets/dummyCa.pem",
 						RefreshDuration: "30m",
 					},
-					tokenCfg:        tokenCfg,
 					token:           token,
 					refreshDuration: dur,
 				},
 				checkfunc: func(actual, expected *svcCertService) bool {
 					return true
 				},
+				wantErr: nil,
 			}
 		}(),
 		func() test {
 			token := func() (string, error) { return "", nil }
-			tokenCfg := config.Token{}
 
 			return test{
 				name: "Fail to parse RefreshDuration",
 				args: args{
 					cfg: config.Config{
-						Token: tokenCfg,
+						Token: config.Token{
+							PrivateKeyPath: "./assets/dummyServer.key",
+						},
 						ServiceCert: config.ServiceCert{
 							AthenzRootCA:    "./assets/dummyCa.pem",
 							RefreshDuration: "",
@@ -79,25 +81,73 @@ func TestNewSvcCertService(t *testing.T) {
 						AthenzRootCA:    "./assets/dummyCa.pem",
 						RefreshDuration: "",
 					},
-					tokenCfg:        tokenCfg,
 					token:           token,
 					refreshDuration: defaultSvcCertRefreshDuration,
 				},
 				checkfunc: func(actual, expected *svcCertService) bool {
 					return true
 				},
+				wantErr: nil,
 			}
 		}(),
 		func() test {
-			dur, _ := time.ParseDuration("30m")
 			token := func() (string, error) { return "", nil }
-			tokenCfg := config.Token{}
 
 			return test{
-				name: "AthenzRootCA file dose not exist",
+				name: "Private key file does not exist",
 				args: args{
 					cfg: config.Config{
-						Token: tokenCfg,
+						Token: config.Token{
+							PrivateKeyPath: "/not/exist.key",
+						},
+						ServiceCert: config.ServiceCert{
+							AthenzRootCA:    "./assets/dummyCa.pem",
+							RefreshDuration: "30m",
+						},
+					},
+					token: token,
+				},
+				want:    &svcCertService{},
+				wantErr: ErrLoadPrivateKey,
+				checkfunc: func(actual, expected *svcCertService) bool {
+					return true
+				},
+			}
+		}(),
+		func() test {
+			token := func() (string, error) { return "", nil }
+
+			return test{
+				name: "Private key is invalid",
+				args: args{
+					cfg: config.Config{
+						Token: config.Token{
+							PrivateKeyPath: "./assets/invalid_dummyServer.key",
+						},
+						ServiceCert: config.ServiceCert{
+							AthenzRootCA:    "./assets/dummyCa.pem",
+							RefreshDuration: "30m",
+						},
+					},
+					token: token,
+				},
+				want:    &svcCertService{},
+				wantErr: ErrFailedToInitialize,
+				checkfunc: func(actual, expected *svcCertService) bool {
+					return true
+				},
+			}
+		}(),
+		func() test {
+			token := func() (string, error) { return "", nil }
+
+			return test{
+				name: "AthenzRootCA file does not exist",
+				args: args{
+					cfg: config.Config{
+						Token: config.Token{
+							PrivateKeyPath: "./assets/dummyServer.key",
+						},
 						ServiceCert: config.ServiceCert{
 							AthenzRootCA:    "/not/exist.pem",
 							RefreshDuration: "30m",
@@ -105,48 +155,10 @@ func TestNewSvcCertService(t *testing.T) {
 					},
 					token: token,
 				},
-				want: &svcCertService{
-					cfg: config.ServiceCert{
-						AthenzRootCA:    "/not/exist.pem",
-						RefreshDuration: "30m",
-					},
-					tokenCfg:        tokenCfg,
-					token:           token,
-					refreshDuration: dur,
-				},
+				want:    &svcCertService{},
+				wantErr: ErrFailedToInitialize,
 				checkfunc: func(actual, expected *svcCertService) bool {
-					return actual.httpClient == http.DefaultClient
-				},
-			}
-		}(),
-		func() test {
-			dur, _ := time.ParseDuration("30m")
-			token := func() (string, error) { return "", nil }
-			tokenCfg := config.Token{}
-
-			return test{
-				name: "AthenzRootCA dose not match x509",
-				args: args{
-					cfg: config.Config{
-						Token: tokenCfg,
-						ServiceCert: config.ServiceCert{
-							AthenzRootCA:    "./assets/invalid_dummyCa.pem",
-							RefreshDuration: "30m",
-						},
-					},
-					token: token,
-				},
-				want: &svcCertService{
-					cfg: config.ServiceCert{
-						AthenzRootCA:    "./assets/invalid_dummyCa.pem",
-						RefreshDuration: "30m",
-					},
-					tokenCfg:        tokenCfg,
-					token:           token,
-					refreshDuration: dur,
-				},
-				checkfunc: func(actual, expected *svcCertService) bool {
-					return actual.httpClient == http.DefaultClient
+					return true
 				},
 			}
 		}(),
@@ -154,16 +166,19 @@ func TestNewSvcCertService(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			actual := NewSvcCertService(tt.args.cfg, tt.args.token)
-			if actual == nil {
-				t.Errorf("TestNewSvcCertService failed expected: %v, actual: %v", tt.want, actual)
+			actual, err := NewSvcCertService(tt.args.cfg, tt.args.token)
+
+			if err != tt.wantErr {
+				t.Errorf("TestNewSvcCertService failed. expected error: %v, actual error: %v", tt.wantErr, err)
+			}
+			if err != nil {
+				return
 			}
 
 			actualSvcCertService := actual.(*svcCertService)
 			expectedSvcCertService := tt.want.(*svcCertService)
 
 			if (actualSvcCertService.cfg != expectedSvcCertService.cfg) ||
-				(actualSvcCertService.tokenCfg != expectedSvcCertService.tokenCfg) ||
 				(actualSvcCertService.refreshDuration != expectedSvcCertService.refreshDuration) ||
 				!tt.checkfunc(actualSvcCertService, expectedSvcCertService) {
 				t.Errorf("TestNewSvcCertService failed expected: %+v, actual: %+v", expectedSvcCertService, actualSvcCertService)

4894dd4158c94e1810cc8c63b7085a8f7961f06a
Katsuya Yamaguchi <katyamag@yahoo-corp.jp>
2019-09-19 13:46:28 +0900

    add domain validator

--- service/svccert.go
+++ service/svccert.go
@@ -134,6 +134,14 @@ func NewSvcCertService(cfg config.Config, token ntokend.TokenProvider) (SvcCertS
 	}, nil
 }
 
+func isValidDomain(domain string) bool {
+	if !domainReg.Copy().MatchString(domain) {
+		return false
+	}
+
+	return true
+}
+
 func setup(cfg config.Config) (*requestTemplate, *zts.ZTSClient, error) {
 	// load private key
 	keyBytes, err := ioutil.ReadFile(cfg.Token.PrivateKeyPath)
@@ -152,7 +160,7 @@ func setup(cfg config.Config) (*requestTemplate, *zts.ZTSClient, error) {
 	// it is used, not the CA. So, we will always put the Athenz name in the CN
 	// (it is *not* a DNS domain name), and put the host name into the SAN.
 
-	if !domainReg.Copy().MatchString(cfg.Token.AthenzDomain) {
+	if !isValidDomain(cfg.Token.AthenzDomain) {
 		return nil, nil, ErrInvalidParameter
 	}
 
@@ -303,7 +311,7 @@ func (s *svcCertService) StartSvcCertUpdater(ctx context.Context) SvcCertService
 				_, err = s.refreshSvcCert()
 				if err != nil {
 					glg.Error(err)
-					time.Sleep(time.Hour)
+					time.Sleep(time.Minute * 10)
 					fch <- struct{}{}
 				}
 			case <-ticker.C:
--- service/svccert_test.go
+++ service/svccert_test.go
@@ -21,6 +21,44 @@ func init() {
 	glg.Get().SetMode(glg.NONE)
 }
 
+func TestIsValidDomain(t *testing.T) {
+	cases := []struct {
+		domain string
+		expect bool
+	}{
+		{
+			domain: "test.domain",
+			expect: true,
+		},
+		{
+			domain: "_testtttdomain.example",
+			expect: true,
+		},
+		{
+			domain: "Top.level-domain",
+			expect: true,
+		},
+		{
+			domain: "01domain",
+			expect: false,
+		},
+		{
+			domain: "-sample.domain",
+			expect: false,
+		},
+		{
+			domain: `%x%sdomain`,
+			expect: false,
+		},
+	}
+
+	for _, c := range cases {
+		if c.expect != isValidDomain(c.domain) {
+			t.Errorf("Failed to validate : %s", c.domain)
+		}
+	}
+}
+
 func TestNewSvcCertService(t *testing.T) {
 	type args struct {
 		cfg   config.Config

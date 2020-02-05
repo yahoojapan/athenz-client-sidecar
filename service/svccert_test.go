2f4301ced810e02e5db15346b2e72b0aa6821a34
Katsuya Yamaguchi <katyamag@yahoo-corp.jp>
2020-02-05 16:07:12 +0900

    add test

--- service/svccert_test.go
+++ service/svccert_test.go
@@ -116,6 +116,48 @@ func TestNewSvcCertService(t *testing.T) {
 		}(),
 		func() test {
 			dur, _ := time.ParseDuration("30m")
+			token := func() (string, error) { return "", nil }
+
+			largeExpiration := 2557920
+
+			return test{
+				name: "Success to initialize SvcCertService when max expiration for certificate",
+				args: args{
+					cfg: config.Config{
+						Token: config.Token{
+							AthenzDomain:   "test.domain",
+							PrivateKeyPath: "./assets/dummyServer.key",
+						},
+						ServiceCert: config.ServiceCert{
+							AthenzRootCA:    "./assets/dummyCa.pem",
+							RefreshDuration: "30m",
+							Expiration:      fmt.Sprintf("%dh", largeExpiration),
+						},
+					},
+					token: token,
+				},
+				want: &svcCertService{
+					cfg: config.ServiceCert{
+						AthenzRootCA:    "./assets/dummyCa.pem",
+						RefreshDuration: "30m",
+						Expiration:      fmt.Sprintf("%dh", largeExpiration),
+					},
+					refreshRequest: &requestTemplate{
+						req: &zts.InstanceRefreshRequest{
+							ExpiryTime: &defaultExpiration,
+						},
+					},
+					token:           token,
+					refreshDuration: dur,
+				},
+				checkfunc: func(actual, expected *svcCertService) bool {
+					return true
+				},
+				wantErr: nil,
+			}
+		}(),
+		func() test {
+			dur, _ := time.ParseDuration("30m")
 			beforeDur, _ := time.ParseDuration("1h")
 			token := func() (string, error) { return "", nil }
 

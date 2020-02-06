e21dfd60e2dd916ab79519d14ac4edcdf289a559
Katsuya Yamaguchi <katyamag@yahoo-corp.jp>
2020-02-06 11:51:35 +0900

    revise comment

--- service/svccert.go
+++ service/svccert.go
@@ -125,8 +125,8 @@ func NewSvcCertService(cfg config.Config, token ntokend.TokenProvider) (SvcCertS
 	}
 
 	// NOTE: The limitation for this expiration is not defined,
-	// but it return error if user sets the exceeded value by int64 (expect less than "1<<63 - 1").
-	// The maximum 'cfg.ServiceCert.Expiration' is approximately 290 years
+	// but it returns error if user sets the exceeded value by int64 (expect less than "1<<63 - 1").
+	// The maximum 'cfg.ServiceCert.Expiration' is approximately 290 years.
 	expireDur, err := time.ParseDuration(cfg.ServiceCert.Expiration)
 	var expireInt int32
 	if err != nil {
